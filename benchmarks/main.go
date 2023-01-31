2c2cbcc51b9882bc0409e5896c06729e629d72b4
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2023-01-31 23:37:07 +0530

    init

---
+++ benchmarks/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	zenq "github.com/alphadose/liteq"
+)
+
+// Example item which we will be writing to and reading from the queue
+type Payload struct {
+	first   byte
+	second  int64
+	third   float64
+	fourth  string
+	fifth   complex64
+	sixth   []rune
+	seventh bool
+}
+
+func NewPayload() *Payload {
+	return &Payload{
+		first:  1,
+		second: 2,
+		third:  3.0,
+		fourth: "4",
+		fifth:  3 + 4i,
+		sixth:  []rune("🐈⚔️👍🌏💥🦖"),
+	}
+}
+
+const (
+	bufferSize = 1 << 14
+)
+
+var (
+	pl Payload = *NewPayload()
+
+	currSize uint64 = throughput[0]
+
+	// input batch size
+	throughput = []uint64{60, 600, 6e3, 6e6, 6e8}
+	// throughput = []uint64{5}
+
+	// Number of writers/producers which will be writing to the queue concurrently
+	numConcurrentWriters uint64 = 1
+
+	// native channel
+	ch chan Payload = make(chan Payload, bufferSize)
+
+	// ZenQ
+	zq *zenq.ZenQ[Payload] = zenq.New[Payload]()
+)
+
+func validatePayload(param Payload) {
+	if param.first != pl.first || param.second != pl.second || param.third != pl.third || param.fourth != pl.fourth || param.fifth != pl.fifth || len(param.sixth) != len(pl.sixth) || param.seventh != pl.seventh {
+		panic("Loss of data integrity")
+	}
+}
+
+func chanProducer() {
+	epochs := currSize / numConcurrentWriters
+	for i := uint64(0); i < epochs; i++ {
+		ch <- pl
+	}
+}
+
+func chanConsumer() {
+	for i := uint64(0); i < currSize; i++ {
+		validatePayload(<-ch)
+	}
+}
+
+func chanRunner() {
+	for i := uint64(0); i < numConcurrentWriters; i++ {
+		go chanProducer()
+	}
+	chanConsumer()
+}
+
+func zenqProducer() {
+	epochs := currSize / numConcurrentWriters
+	for i := uint64(0); i < epochs; i++ {
+		zq.Write(pl)
+	}
+}
+
+func zenqConsumer() {
+	for i := uint64(0); i < currSize; i++ {
+		validatePayload(zq.Read())
+	}
+}
+
+func zenqRunner() {
+	for i := uint64(0); i < numConcurrentWriters; i++ {
+		go zenqProducer()
+	}
+	zenqConsumer()
+}
+
+func measureTime(callback func(), runnerName string) {
+	var startTime time.Time = time.Now()
+	callback()
+	fmt.Printf("%s Runner completed transfer in: %v\n", runnerName, time.Since(startTime))
+}
+
+// drain the channel and zenQ
+func cleanup() {
+	for len(ch) > 0 {
+		<-ch
+	}
+}
+
+func main() {
+	cleanup()
+	for _, tput := range throughput {
+		currSize = tput
+		fmt.Printf("With Input Batch Size: %d and Num Concurrent Writers: %d\n", currSize, numConcurrentWriters)
+		fmt.Print("\n")
+
+		// Run tests
+		measureTime(chanRunner, "Native Channel")
+		measureTime(zenqRunner, "ZenQ")
+		fmt.Print("====================================================================\n\n")
+	}
+}
---
+++ zenq.go
@@ -0,0 +1,94 @@
+// A minimalist thread-safe queue implemented using a lock-free ringbuffer which is faster than golang's native channels
+// Based on the LMAX disruptor pattern https://lmax-exchange.github.io/disruptor/disruptor.html
+
+// Suggestions:-
+//
+// Use runtime.LockOSThread() on the goroutine calling ZenQ.Read() for lowest latency provided you have > 1 cpu cores
+
+package zenq
+
+import (
+	"fmt"
+	"runtime"
+	"sync/atomic"
+	"unsafe"
+
+	"github.com/alphadose/liteq/constants"
+)
+
+// ZenQ Slot state enums
+const (
+	SlotEmpty = iota
+	SlotBusy
+	SlotCommitted
+)
+
+const (
+	// queue size must be a power of 2, max size-> 2^31
+	size      uint32 = 1 << 14
+	indexMask uint32 = size - 1
+)
+
+type (
+	// a single slot in the queue
+	slot[T any] struct {
+		atomic.Uint32
+		item T
+	}
+
+	// ZenQ ringbuffer
+	ZenQ[T any] struct {
+		_           [constants.CacheLinePadSize]byte
+		writerIndex atomic.Uint32
+		_           [constants.CacheLinePadSize - unsafe.Sizeof(atomic.Uint32{})]byte
+		readerIndex atomic.Uint32
+		_           [constants.CacheLinePadSize - unsafe.Sizeof(atomic.Uint32{})]byte
+		contents    [size]slot[T]
+		_           [constants.CacheLinePadSize]byte
+	}
+)
+
+// New returns a new queue given its payload type passed as a generic parameter
+func New[T any]() *ZenQ[T] {
+	return &ZenQ[T]{}
+}
+
+// Write a value to the queue
+func (self *ZenQ[T]) Write(value T) {
+	slot := &self.contents[self.writerIndex.Add(1)&indexMask]
+	for !slot.CompareAndSwap(SlotEmpty, SlotBusy) {
+		switch slot.Load() {
+		case SlotBusy, SlotCommitted:
+			runtime.Gosched()
+		case SlotEmpty:
+			continue
+		}
+	}
+	slot.item = value
+	slot.Store(SlotCommitted)
+}
+
+// Read a value from the queue
+func (self *ZenQ[T]) Read() (data T) {
+	slot := &self.contents[self.readerIndex.Add(1)&indexMask]
+	for !slot.CompareAndSwap(SlotCommitted, SlotBusy) {
+		switch slot.Load() {
+		case SlotBusy, SlotEmpty:
+			runtime.Gosched()
+		case SlotCommitted:
+			continue
+		}
+	}
+	data = slot.item
+	slot.Store(SlotEmpty)
+	return
+}
+
+// Dump dumps the current queue state
+// Unsafe to be called from multiple goroutines
+func (self *ZenQ[T]) Dump() {
+	fmt.Printf("writerIndex: %3d, readerIndex: %3d\n contents:-\n\n", self.writerIndex, self.readerIndex)
+	for idx := range self.contents {
+		fmt.Printf("Slot -> %#v\n", self.contents[idx])
+	}
+}
